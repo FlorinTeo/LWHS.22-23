1b1172290656179d6589584801945c42bbcc060a
Florin.Teo <Florin.Teo@live.com>
2022-09-04 18:09:58 -0700

    Adding HelloJavaWorld

---
+++ HelloJavaWorld/src/HelloJavaWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloJavaWorld {
+
+	/**
+	 * Bootstrapping project for Java work
+	 * @param args - command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Java World!");
+	}
+
+}
