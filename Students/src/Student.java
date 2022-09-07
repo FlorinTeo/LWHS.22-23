a4a5dc964be979660c8ac06b2f0e5fd5eebedd1a
Florin.Teo <Florin.Teo@live.com>
2022-09-07 07:57:57 -0700

    Testing Student

---
+++ Students/src/Student.java
@@ -0,0 +1,90 @@
+/**
+ * Class representing a record of a highschool student
+ */
+public class Student {
+    
+    // Student ID as given by the school
+    private int id;
+    
+    // Student's name
+    private String name;
+    
+    // Student grade: one of 9, 10, 11 or 12.
+    private int grade;
+    
+    /**
+     * Creates and initializes an instance of a student.
+     * @param id - student's ID as given by school.
+     * @param name - student's first name.
+     * @param grade - the grade the student is in (9, 10, 11 or 12).
+     */
+    public Student(int id, String name, int grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+    
+    /**
+     * Class accessor for the ID.
+     * @return student's ID.
+     */
+    public int getId() {
+        return id;
+    }
+    
+    /**
+     * Class accessor for the name.
+     * @return student's name.
+     */
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * Class accessor for the grade.
+     * @return The grade this student is in.
+     */
+    public int getGrade() {
+        return grade;
+    }
+    
+    /**
+     * Class mutator allowing adjustments of the student's name.
+     * @param name - The new name for this student.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Gives the string representation of this student.
+     */
+    @Override
+    public String toString() {
+        String output = "[" + id + "]";
+        output += " " + name;
+        output += " in grade " + grade;
+        return output;
+    }
+    
+    /**
+     * Check's if this student's instance is valid:
+     * id should be a positive number
+     * name should be a non-null string
+     * grade should be one of the 9, 10, 11 or 12
+     * @return - true if instance is valid, false otherwise
+     */
+    public boolean isValid() {
+        if (id <= 0) {
+            return false;
+        }
+        if (name.isBlank() || name.isEmpty()) {
+            return false;
+        }
+        if (grade < 9 || grade > 12) {
+            return false;
+        }
+        
+        return true;
+    }
+}
---
+++ Students/tests/Student_tests.java
@@ -0,0 +1,67 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class Student_tests {
+
+    @Test
+    void testConstructor() {
+        // Tests a properly created student instance
+        // returns the expected string
+        Student fiona = new Student(123, "Fiona Fantastic", 9);
+        Assert.assertEquals("[123] Fiona Fantastic in grade 9", fiona.toString());
+    }
+    
+    @Test
+    void testAccessors() {
+        // Tests that a properly created student instance
+        // returns the expected values for each of its accessors
+        Student mary = new Student(451, "Mary Marvellous", 12);
+        Assert.assertEquals(451, mary.getId());
+        Assert.assertEquals("Mary Marvellous", mary.getName());
+        Assert.assertEquals(12, mary.getGrade());
+    }
+    
+    @Test
+    void testMutator() {
+        // Tests that a properly created student instance
+        // can be modified via its mutator and the modified value is visible via the accessor
+        Student brian = new Student(743, "Brian Briliant", 11);
+        Assert.assertNotEquals("Brian Brilliant", brian.getName());
+        brian.setName("Brian Brilliant");
+        Assert.assertEquals("Brian Brilliant", brian.getName());
+    }
+    
+    @Test
+    void testIsValid() {
+        // Tests a student with an empty or blank name is correctly marked as invalid
+        // Once the name is set to something non blank/empty then instance becomes valid
+        Student sean = new Student(852, "", 10);
+        Assert.assertFalse(sean.isValid());
+        sean.setName(" ");
+        Assert.assertFalse(sean.isValid());
+        sean.setName("Sean Superstar");
+        Assert.assertTrue(sean.isValid());
+
+        // Tests a student with a 0 or negative id is invalid
+        // A student with an id as small as 1 becomes valid
+        Student ulisse = new Student(-1, "Ulisse Unknown", 9);
+        Assert.assertFalse(ulisse.isValid());
+        ulisse = new Student(0, "Ulisse Unknown", 9);
+        Assert.assertFalse(ulisse.isValid());
+        ulisse = new Student(1, "Ulisse Unknown", 9);
+        Assert.assertTrue(ulisse.isValid());
+        
+        // Tests only students with a legal grade (9, 10, 11 or 12) are seen as
+        // valid, all the rest are properly detected as invalid.
+        for (int grade = -10; grade <= 30; grade++) {
+            if (grade >= 9 && grade <= 12) {
+                Student ines = new Student(311, "Ines InHighschool", grade);
+                Assert.assertTrue(ines.isValid());
+            } else {
+                Student nick = new Student(311, "Nick NotInHighschool", 13);
+                Assert.assertFalse(nick.isValid());
+            }
+        }
+    }
+
+}
