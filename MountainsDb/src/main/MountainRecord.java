39357afbdc1b22a3ab2aad39f3e2c53e04e91b8e
Florin.Teo <Florin.Teo@live.com>
2022-09-19 07:15:45 -0700

    Untabify

--- MountainsDb/src/main/MountainRecord.java
+++ MountainsDb/src/main/MountainRecord.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MountainRecord {
 
-	private String _rawLine;
+    private String _rawLine;
     private String _country;
     private String _type;
     private String _name;
@@ -12,16 +12,15 @@ public class MountainRecord {
     private double _longitude;
     private double _elevation;
     private String _elevationUnit;
-    
+
     public MountainRecord(String line) {
-    	_rawLine = line;
+        _rawLine = line;
         String[] lineParts = _rawLine.split("\t");
         if (lineParts.length != 6) {
             throw new RuntimeException(
-                    String.format("Corrupted db record: [%d] parts instead of 6.",
-                            lineParts.length));
+                    String.format("Corrupted db record: [%d] parts instead of 6.", lineParts.length));
         }
-        
+
         _country = lineParts[0];
         _type = lineParts[1];
         _name = lineParts[2];
@@ -33,37 +32,32 @@ public class MountainRecord {
         elevParser.close();
         if (!_elevationUnit.equalsIgnoreCase("m") && !_elevationUnit.equalsIgnoreCase("ft")) {
             throw new RuntimeException(
-                    String.format("Corrupted db record: '%s' elevation unit is unrecognized.",
-                            _elevationUnit));
+                    String.format("Corrupted db record: '%s' elevation unit is unrecognized.", _elevationUnit));
         }
     }
-    
+
     public String getRawLine() {
-    	return _rawLine;
+        return _rawLine;
     }
-    
+
     public double getElevation(String elevationUnit) {
         if (!elevationUnit.equalsIgnoreCase("m") && !elevationUnit.equalsIgnoreCase("ft")) {
             throw new RuntimeException(
-                    String.format("Invalid request: '%s' elevation unit is unrecognized.",
-                            elevationUnit));
+                    String.format("Invalid request: '%s' elevation unit is unrecognized.", elevationUnit));
         }
-        
+
         double factor = 1;
         if (!elevationUnit.equalsIgnoreCase(_elevationUnit)) {
-        	factor = _elevationUnit.equalsIgnoreCase("m") 
-        			? 3.2808399 // meters to feet 
-        			: 0.3048 ;// feet to meters
+            factor = _elevationUnit.equalsIgnoreCase("m") ? 3.2808399 // meters to feet
+                    : 0.3048;// feet to meters
         }
-        
+
         return _elevation * factor;
     }
-    
+
     @Override
     public String toString() {
-    	return String.format("%s %s in %s [lat:%f, lon:%f], elevation %f ft",
-    			_type, _name,
-    			_country, _latitude, _longitude,
-    			getElevation("ft"));
+        return String.format("%s %s in %s [lat:%f, lon:%f], elevation %f ft", _type, _name, _country, _latitude,
+                _longitude, getElevation("ft"));
     }
 }
--- MountainsDb/src/main/MountainsDb.java
+++ MountainsDb/src/main/MountainsDb.java
@@ -14,7 +14,7 @@ public class MountainsDb {
     private Path _absolutePath;
     private long _size;
     private long _nLines;
-    
+
     public MountainsDb(String dbFilePath) {
         _dbPath = Paths.get(dbFilePath);
         _absolutePath = null;
@@ -22,7 +22,7 @@ public class MountainsDb {
         _size = -1;
         _nLines = -1;
     }
-    
+
     public void scan() throws IOException {
         if (!_scanned) {
             System.out.print("Scanning ... ");
@@ -30,104 +30,103 @@ public class MountainsDb {
             _absolutePath = _dbPath.toAbsolutePath();
             _size = Files.size(_dbPath);
             _nLines = 0;
-            while(reader.ready()) {
+            while (reader.ready()) {
                 String line = reader.readLine();
                 _nLines++;
-                
+
                 // skip the header line
                 if (_nLines == 1) {
                     continue;
                 }
-                
+
                 try {
                     new MountainRecord(line);
                 } catch (RuntimeException re) {
-                    throw new RuntimeException(
-                        String.format("[%,d] %s", _nLines, re.getMessage()));
+                    throw new RuntimeException(String.format("[%,d] %s", _nLines, re.getMessage()));
                 }
             }
-            
+
             _scanned = true;
             System.out.println("DONE");
         }
     }
-    
+
     public void split() throws IOException {
-    	scan();
+        scan();
         BufferedReader reader = Files.newBufferedReader(_dbPath, StandardCharsets.UTF_8);
         BufferedWriter writer1 = Files.newBufferedWriter(Paths.get(_dbPath + "_1"), StandardCharsets.UTF_8);
         BufferedWriter writer2 = Files.newBufferedWriter(Paths.get(_dbPath + "_2"), StandardCharsets.UTF_8);
         int nLine = 0;
-        while(reader.ready()) {
+        while (reader.ready()) {
             String line = reader.readLine();
-            
+
             // write the header line and all subsequent even lines
             if (nLine % 2 == 0) {
-            	writer1.write(line + "\n");
-            } 
-            
+                writer1.write(line + "\n");
+            }
+
             // write the header line and all subsequent odd lines
             if (nLine == 0 || nLine % 2 == 1) {
-            	writer2.write(line + "\n");
+                writer2.write(line + "\n");
             }
             nLine++;
         }
-        
+
         writer2.close();
         writer1.close();
     }
-    
-    public void merge( ) throws IOException {
-    	BufferedReader reader1 = Files.newBufferedReader(Paths.get(_dbPath + "_1"), StandardCharsets.UTF_8);
+
+    public void merge() throws IOException {
+        BufferedReader reader1 = Files.newBufferedReader(Paths.get(_dbPath + "_1"), StandardCharsets.UTF_8);
         BufferedReader reader2 = Files.newBufferedReader(Paths.get(_dbPath + "_2"), StandardCharsets.UTF_8);
         BufferedWriter writer = Files.newBufferedWriter(Paths.get(_dbPath + "_merged"), StandardCharsets.UTF_8);
-        
+
         String header = reader1.readLine();
         reader2.readLine();
         writer.write(header + "\n");
-        
+
         MountainRecord rec1 = null;
         MountainRecord rec2 = null;
-        while(rec1 == null || rec2 == null) {
-        	if (rec1 == null) {
-        		rec1 = new MountainRecord(reader1.readLine());
-        	}
-        	if (rec2 == null) {
-        		rec2 = new MountainRecord(reader2.readLine());
-        	}
-        	
-        	if (rec1.getElevation("m") < rec2.getElevation("m")) {
-        		writer.write(rec1.getRawLine() + "\n");
-        		if (reader1.ready()) {
-        			rec1 = null;
-        		}
-        	} else {
-        		writer.write(rec2.getRawLine() + "\n");
-        		if (reader2.ready()) {
-        			rec2 = null;
-        		}
-        	}
+        while (rec1 == null || rec2 == null) {
+            if (rec1 == null) {
+                rec1 = new MountainRecord(reader1.readLine());
+            }
+            if (rec2 == null) {
+                rec2 = new MountainRecord(reader2.readLine());
+            }
+
+            if (rec1.getElevation("m") < rec2.getElevation("m")) {
+                writer.write(rec1.getRawLine() + "\n");
+                if (reader1.ready()) {
+                    rec1 = null;
+                }
+            } else {
+                writer.write(rec2.getRawLine() + "\n");
+                if (reader2.ready()) {
+                    rec2 = null;
+                }
+            }
         }
-        
-        while(reader1.ready()) {
-        	writer.write(reader1.readLine() + "\n");
+
+        while (reader1.ready()) {
+            writer.write(reader1.readLine() + "\n");
         }
-        
-        while(reader2.ready()) {
-        	writer.write(reader2.readLine() + "\n");
+
+        while (reader2.ready()) {
+            writer.write(reader2.readLine() + "\n");
         }
-        
+
         writer.close();
         reader1.close();
         reader2.close();
         Files.delete(Paths.get(_dbPath + "_1"));
         Files.delete(Paths.get(_dbPath + "_2"));
     }
-    
+
     @Override
     public String toString() {
         String output = String.format("Database '%s':\n", _dbPath);
-        
+
         try {
             scan();
             output += String.format("  Path: '%s'\n", _absolutePath);
@@ -136,7 +135,7 @@ public class MountainsDb {
         } catch (Exception e) {
             output += String.format("  ##ERROR##: %s\n", e);
         }
-        
+
         return output;
     }
 
