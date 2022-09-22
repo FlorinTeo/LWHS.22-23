d1ca3d6c9bae51c857c8e34af3c850a777c47218
Florin.Teo <Florin.Teo@live.com>
2022-09-21 23:22:14 -0700

    Cosmetics

--- Mountains/src/main/Program.java
+++ Mountains/src/main/Program.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 public class Program {
 
-	public static void main(String[] args) throws IOException {
-		MountainsDb mDb = new MountainsDb("mountains_db.tsv");
-		mDb.clean();
-		System.out.println(mDb);
-	}
+    public static void main(String[] args) throws IOException {
+        MountainsDb mDb = new MountainsDb("mountains_db.tsv");
+        mDb.clean();
+        System.out.println(mDb);
+    }
 
 }
