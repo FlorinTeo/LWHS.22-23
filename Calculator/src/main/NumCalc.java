9a3a9d66dfd8bcfa5ff9bd1ae3047f3f23a8a5fc
Florin.Teo <Florin.Teo@live.com>
2022-10-06 21:13:42 -0700

    Add Calculators

---
+++ Calculator/src/main/NumCalc.java
@@ -0,0 +1,18 @@
+package main;
+
+public class NumCalc {
+    
+    public NumCalc() {
+        // initialize the calculator
+    }
+    
+    public String evaluate(String expression) {
+        // return the String representation of the final evaluation result
+        throw new RuntimeException("Not implemented yet!");
+    }
+    
+    public String toString() {
+        // return the traces of the most recent evaluation result
+        return "";
+    }
+}
--- Calculator/src/main/Program.java
+++ Calculator/src/main/Program.java
@@ -6,17 +6,30 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Smart Calculator!");
+        NumCalc numCalc = new NumCalc();
 
         Scanner input = new Scanner(System.in);
         do {
             System.out.printf("Expression?> ");
-            String line = input.nextLine();
+            String line = input.nextLine().trim();
             if (line.isEmpty()) {
                 continue;
             }
             if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
                 break;
             }
+            
+            try {
+                String result = numCalc.evaluate(line);
+                System.out.printf("%s\n", result);
+            } catch (Exception e) {
+                System.out.println();
+                System.out.printf("##Error: %s\n", e.getMessage());
+                System.out.printf("##Stack: ----------------\n");
+                e.printStackTrace();
+                System.out.println();
+            }
+
         } while (true);
 
         System.out.println("Goodbye!");
