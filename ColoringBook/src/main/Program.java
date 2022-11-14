55dcb457a5a9feb88f21d172bdfe3a7e99a7d29f
Florin.Teo <Florin.Teo@live.com>
2022-11-14 13:10:56 -0800

    Comment solution

--- ColoringBook/src/main/Program.java
+++ ColoringBook/src/main/Program.java
@@ -17,6 +17,11 @@ public class Program {
     private static Drawing _drawing;
     private static DrawingFrame _frame;
     
+    /**
+     * The unit of work for the flooding algorithm is the individual pixel (x,y)
+     * that needs to be colored. Processing this work item may generate other
+     * work items, for the un-colored neighbors of this pixel.
+     */
     public static class WorkItem {
         public int _x;
         public int _y;
@@ -26,14 +31,32 @@ public class Program {
         }
     };
     
+    /**
+     * Iterative implementation of the flooding algorithm, by using a Queue.
+     * This is a <b>breath-first</b> algorithm: flooding is slower but using small
+     * anmount of memory, working on the perimeter of the already colored area.
+     * @param xSeed - the initial x-coordinate where the flooding starts from
+     * @param ySeed - the initial y-coordinate where the flooding starts from
+     * @param col - the color to use for flooding.
+     * @throws InterruptedException
+     */
     public static void floodQ(int xSeed, int ySeed, Color col) throws InterruptedException {
+        // Create the queue, and the first work items.
+        // Color this first pixel then add its work item into the queue
         Queue<WorkItem> queue = new LinkedList<WorkItem>();
         WorkItem work = new WorkItem(xSeed, ySeed);
         _drawing.setPixel(work._x, work._y, col);
         queue.add(work);
+        
+        // keep removing work items from the queue for as long as there's work to do
         while (!queue.isEmpty()) {
-            _frame.step();
+            _frame.step(1);
             _frame.setStatusMessage(String.format("%d",queue.size()));
+            
+            // remove the work item from the queue and execute on it:
+            // analyze each of the neighbors of this pixel and if that neighbor is within
+            // image bounds and needs coloring, color it, then add a new work item for it
+            // into the queue.
             work = queue.remove();
             for(int x = work._x - 1; x <= work._x + 1; x ++) {
                 for (int y = work._y - 1; y <= work._y + 1; y++) {
@@ -49,14 +72,34 @@ public class Program {
         }
     }
     
+    /**
+     * Iterative implementation of the flooding algorithm, by using a Stack.
+     * This is a <b>depth-first</b> algorithm: it makes deeper and faster inroads in the
+     * uncolored areas, but uses more memory to keep track of the path taken already.
+     * It is very similiar to the recursive algorithm, but by using a Stack it avoids
+     * hitting StackOverflowException since it is using heap memory.
+     * @param xSeed - the initial x-coordinate where the flooding starts from
+     * @param ySeed - the initial y-coordinate where the flooding starts from
+     * @param col - the color to use for flooding.
+     * @throws InterruptedException
+     */
     public static void floodS(int xSeed, int ySeed, Color col) throws InterruptedException {
+        // Create the queue, and the first work items.
+        // Color this first pixel then push its work item to the stack
         Stack<WorkItem> stack = new Stack<WorkItem>();
         WorkItem work = new WorkItem(xSeed, ySeed);
         _drawing.setPixel(work._x, work._y, col);
         stack.push(work);
+        
+        // keep extracting work items from the stack for as long as there's work to do
         while (!stack.empty()) {
             _frame.step(1);
             _frame.setStatusMessage(String.format("%d",stack.size()));
+            
+            // pop the work item from the stack and execute on it:
+            // analyze each of the neighbors of this pixel and if that neighbor is within
+            // image bounds and needs coloring, color it, then push a new work item for it
+            // onto the stack.
             work = stack.pop();
             for(int x = work._x - 1; x <= work._x + 1; x ++) {
                 for (int y = work._y - 1; y <= work._y + 1; y++) {
@@ -72,9 +115,21 @@ public class Program {
         }
     }
     
+    /**
+     * Recursive implementation of the flooding algorithm.
+     * @param xSeed - the initial x-coordinate where the flooding starts from
+     * @param ySeed - the initial y-coordinate where the flooding starts from
+     * @param col - the color to use for flooding.
+     * @throws InterruptedException
+     */
     public static void floodR(int xSeed, int ySeed, Color col) throws InterruptedException {
+        // first and foremost, the pixel at xSeed, ySeed needs coloring
         _drawing.setPixel(xSeed, ySeed, col);
-        _frame.step(2);
+        _frame.step(1);
+        
+        // then we can inspect each of its neighbors, and if the neighbor is within the
+        // bounds of the image and needs coloring, we'll recursively ask that pixel to
+        // color itself and propagate the flooding further to its un-colored neighbors.
         for (int x = xSeed - 1; x <= xSeed + 1; x++) {
             for (int y = ySeed - 1; y <= ySeed + 1; y++) {
                 if (x == xSeed && y == ySeed) {
