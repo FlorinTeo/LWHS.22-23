40b9672686d1ab2b6ff81808affd301b6c7b1949
Florin.Teo <Florin.Teo@live.com>
2023-02-09 10:35:56 -0800

    Some testing

--- Graphs/src/Program.java
+++ Graphs/src/Program.java
@@ -2,13 +2,25 @@
 public class Program {
 
     public static void main(String[] args) {
-        Graph<String> g = new Graph<String>();
-        g.addNode("abc");
-        g.addNode("def");
-        g.addEdge("abc", "def");
-        g.addNode("xyz");
-        g.addEdge("xyz", "def");
-        System.out.println(g);
+        Graph<String> gs = new Graph<String>();
+        gs.addNode("abc");
+        gs.addNode("def");
+        gs.addEdge("abc", "def");
+        gs.addNode("xyz");
+        gs.addEdge("xyz", "def");
+        System.out.println(gs);
+        
+        Graph<Integer> gi = new Graph<Integer>();
+        gi.addNode(1);
+        gi.addNode(2);
+        gi.addNode(3);
+        gi.addNode(4);
+        gi.addNode(5);
+        gi.addEdge(1, 2);
+        gi.addEdge(2, 3);
+        gi.addEdge(3, 1);
+        gi.addEdge(4, 5);
+        System.out.println(gi);
     }
 
 }
