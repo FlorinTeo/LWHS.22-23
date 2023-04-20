078f3423fafae98e78d55189ea49285002d020d7
Florin.Teo <Florin.Teo@live.com>
2023-04-19 23:56:23 -0700

    add comments.

--- PrioQueue/src/main/HeapPriorityQueue.java
+++ PrioQueue/src/main/HeapPriorityQueue.java
@@ -53,10 +53,13 @@ public class HeapPriorityQueue<E extends Comparable<E>> implements PriorityQueue
     
     @Override
     public void add(E value) {
+        // add the value to the end of the array and resize as needed
         int i = size + 1;
         checkResize(i);
         elements[i] = value;
         size++;
+        
+        // bubble that value up until the parent becomes smaller 
         while (hasParent(i)) {
             int iParent = parent(i);
             if (elements[iParent].compareTo(elements[i]) <= 0) {
@@ -84,25 +87,39 @@ public class HeapPriorityQueue<E extends Comparable<E>> implements PriorityQueue
 
     @Override
     public E remove() {
+        // return null if there's nothing to remove
         if (isEmpty()) {
             return null;
         }
+        
+        // save the first element, which is the root of the tree, which will be returned
         E elem = elements[1];
+        // bring the last element in the queue (lowest, right most leaf) to the root
         elements[1] = elements[size];
         size--;
+        // push down the root for as long as either of its children is smaller than itself.
+        // the node may have 0, 1 (left) or 2 (left and right) children.
         int i = 1;
         while(hasLeftChild(i)){
+            // node has left child, assume that's what the value will be compared against
             int iChild = leftChild(i);
             if (hasRightChild(i) && elements[rightChild(i)].compareTo(elements[leftChild(i)]) < 0) {
+                // however if there is a right child as well, which is even smaller than the left
+                // will keep this as the child to compare against.
                 iChild = rightChild(i);
             }
-            if (elements[iChild].compareTo(elements[i]) < 0) {
-                swap(i, iChild);
-                i = iChild;
-            } else {
+            
+            // if the child to compare against is already larger than the value, we're done. 
+            if (elements[i].compareTo(elements[iChild]) < 0) {
                 break;
             }
+            
+            // otherwise swap the value with the child's value and keep going.
+            swap(i, iChild);
+            i = iChild;
         }
+        
+        // return the element that was saved at the very beginning.
         return elem;
     }
 
